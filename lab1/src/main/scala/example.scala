971fdb52890654d2439ef3b7c1832c796de223f7
Huizerd <jessehagenaars@gmail.com>
2018-09-11 17:07:40 +0200

    Can read csv

--- lab1/src/main/scala/example.scala
+++ lab1/src/main/scala/example.scala
@@ -3,37 +3,49 @@ package example
 import org.apache.spark.sql.types._
 import org.apache.spark.sql._
 import org.apache.log4j.{Level, Logger}
-import java.sql.Timestamp
+import java.sql.{Date, Timestamp}
 
 
 object ExampleSpark {
-  case class SensorData ( 
-    sensorName: String, 
-    timestamp: Timestamp, 
-    numA: Double,
-    numB: Double,
-    numC: Long,
-    numD: Double,
-    numE: Long,
-    numF: Double
+  case class Data (
+    publishDate: Date,
+    allNames: String
   )
   def main(args: Array[String]) {
     val schema =
       StructType(
         Array(
-          StructField("sensorname", StringType, nullable=false),
-          StructField("timestamp", TimestampType, nullable=false),
-          StructField("numA", DoubleType, nullable=false),
-          StructField("numB", DoubleType, nullable=false),
-          StructField("numC", LongType, nullable=false),
-          StructField("numD", DoubleType, nullable=false),
-          StructField("numE", LongType, nullable=false),
-          StructField("numF", DoubleType, nullable=false)
+          StructField("gkgRecordId", StringType, nullable=true),
+          StructField("publishDate", DateType, nullable=true),
+          StructField("sourceCollectionIdentifier", IntegerType, nullable=true),
+          StructField("sourceCommonName", StringType, nullable=true),
+          StructField("documentIdentifier", StringType, nullable=true),
+          StructField("counts", StringType, nullable=true),
+          StructField("enhancedCounts", StringType, nullable=true),
+          StructField("themes", StringType, nullable=true),
+          StructField("enhancedThemes", StringType, nullable=true),
+          StructField("locations", StringType, nullable=true),
+          StructField("enhancedLocations", StringType, nullable=true),
+          StructField("persons", StringType, nullable=true),
+          StructField("enhancedPersons", StringType, nullable=true),
+          StructField("organisations", StringType, nullable=true),
+          StructField("enhancedOrganisations", StringType, nullable=true),
+          StructField("tone", StringType, nullable=true),
+          StructField("enhancedDates", StringType, nullable=true),
+          StructField("gcams", StringType, nullable=true),
+          StructField("sharingImage", StringType, nullable=true),
+          StructField("relatedImages", StringType, nullable=true),
+          StructField("socialImageEmbeds", StringType, nullable=true),
+          StructField("socialVideoEmbeds", StringType, nullable=true),
+          StructField("quotations", StringType, nullable=true),
+          StructField("allNames", StringType, nullable=true),
+          StructField("amounts", StringType, nullable=true),
+          StructField("translationInfo", StringType, nullable=true),
+          StructField("extrasXML", StringType, nullable=true)
         )
       )
 
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
-
 
     val spark = SparkSession
       .builder
@@ -45,15 +57,14 @@ object ExampleSpark {
     import spark.implicits._
 
     val ds = spark.read 
-                  .schema(schema) 
-                  .option("timestampFormat", "MM/dd/yy:hh:mm")
-                  .csv("./sensordata.csv")
-                  .as[SensorData]
+                  .schema(schema)
+                  .option("delimiter", "\t")
+                  .option("dateFormat", "yyyyMMddhhmmss")
+                  .csv("/home/huis/Projects/ET4310_SBD/data/segment/*.csv")
+                  .select("publishDate", "allNames")
+                  .as[Data]
 
-    val dsFilter = ds.filter(a => a.timestamp ==
-        new Timestamp(2014 - 1900, 2, 10, 1, 1, 0, 0))
-
-    dsFilter.collect.foreach(println)
+    ds.take(10).foreach(println)
 
     spark.stop
   }
