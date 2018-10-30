14659088ffbeb34c6b256316b0608ad8fd805c8f
Olle <olle.eriksson94@gmail.com>
2018-10-30 17:56:22 +0100

    some changes to first version, added another version as well (2)

--- lab3/GDELTStream2/GDELTStream.scala
+++ lab3/GDELTStream2/GDELTStream.scala
@@ -31,6 +31,15 @@ object GDELTStream extends App {
 
   builder.addStateStore(keyValueStoreBuilder)
 
+  val keyValueStoreBuilder2 = Stores.keyValueStoreBuilder(
+    Stores.inMemoryKeyValueStore("timeStore"),
+    Serdes.String,
+    Serdes.Long
+  )
+
+  builder.addStateStore(keyValueStoreBuilder2)
+
+
   val records: KStream[String, String] = builder.stream[String, String]("gdelt")
     .filter((k, v) => v.split("\t", -1).length == 27) // check correct length
     .flatMap((k,v) => {
@@ -44,7 +53,7 @@ object GDELTStream extends App {
       .filter(x => x._2 != "" && x._2 != "Type ParentCategory") // filter for bad names
     })
 
-  var r2 = records.transform(new HistogramTransformer(), "countStore")
+  var r2 = records.transform(new HistogramTransformer(), "countStore","timeStore")
   r2.to("gdelt-histogram")
 
   val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
@@ -61,14 +70,16 @@ object GDELTStream extends App {
 }
 
 class HistogramTransformer extends Transformer[String, String, (String, Long)] {
+
   var context: ProcessorContext = _
   var kvStoreCount: KeyValueStore[String, Long] = _
+  var kvStoreTime: KeyValueStore[String, Long] = _
   var hourInMs = 3600*1000
 
-  // must maybe clear the store, so not get out of memory
   def init(context: ProcessorContext) {
     this.context = context
     this.kvStoreCount = context.getStateStore("countStore").asInstanceOf[KeyValueStore[String, Long]]
+    this.kvStoreTime = context.getStateStore("timeStore").asInstanceOf[KeyValueStore[String, Long]]
 
     // send current result downStream every 1s
     this.context.schedule(1000, PunctuationType.STREAM_TIME, (timestamp) => {
@@ -81,32 +92,23 @@ class HistogramTransformer extends Transformer[String, String, (String, Long)] {
          // commit the current processing progress
          context.commit()
      })
-
-     // reset counts every 1h
-     // problem: unclear how this schedule-method works.
-     // problem: this assumes time starts when starts to run program
-     // alt.: delete the store, and initialize it again (no mem. problem? possible?)
-     this.context.schedule(hourInMs, PunctuationType.WALL_CLOCK_TIME, (timestamp)=>{
-        val iter = this.kvStoreCount.all
-        while(iter.hasNext) {
-          val entry = iter.next()
-          this.kvStoreCount.put(entry.key, 0)
-        }
-     })
   }
-
   def transform(key: String, name: String): (String, Long) = {
     val oldCount: Long = this.kvStoreCount.get(name)
     val existing = Option(oldCount)
+    val currTime = context.timestamp
 
-    // alt.: one more kvStore storing the time each message came. Update this time
-    //       when there has been more than 1 hour since last it came to transform.
-    //       => problem: count isn't reset until next time key (msg). Should be res. earlier
-
-    if(existing == None){ // initialize
+    if(existing == None){ //initialize
       this.kvStoreCount.put(name, 1)
-    }else{ // update
-      this.kvStoreCount.put(name, oldCount+1)
+      this.kvStoreTime.put(name, currTime)
+    }else{
+      val createTime = this.kvStoreTime.get(name)
+      if(currTime-createTime>hourInMs){ // reset
+        this.kvStoreCount.put(name, 1)
+        this.kvStoreTime.put(name, currTime)
+      }else{ // update
+        this.kvStoreCount.put(name, oldCount+1)
+      }
     }
     (name, this.kvStoreCount.get(name))
   }
